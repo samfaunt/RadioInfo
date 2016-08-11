38d679cc9d8924377808d1a0204c92c7aece7f78
samfaunt <samfty@gmail.com>
2016-08-11 20:43:21 +0930

    Added Mac Address

--- snmp.php
+++ snmp.php
@@ -2,6 +2,9 @@
 
 $stationnamesnmp = snmpwalk("10.106.8.1", "public", ".1.3.6.1.4.1.41112.1.4.7.1.2");
 $signalsnmp = snmpwalk("10.106.8.1", "public", ".1.3.6.1.4.1.41112.1.4.7.1.3");
+$stationmacsnmp = snmpwalk("10.106.8.1","public",".1.3.6.1.4.1.41112.1.4.7.1.1");
+$stationTxRate = snmpwalk("10.106.8.1", "public",".1.3.6.1.4.1.41112.1.4.7.1.17");
+
 
 
 foreach ($signalsnmp as $value) {
@@ -17,12 +20,36 @@ foreach ($stationnamesnmp as $value) {
 	$stationname[] = $str;
 }
 
+foreach ($stationmacsnmp as $value){
+    $str = $value;
+    if (strpos($str, '"') !== false ){
+        $str = substr($str, 9, -1);
+        $str = bin2hex($str);
+        $str = chunk_split($str,2,' ');
+        $str = strtoupper($str);
+        $stationmac[] = $str;
 
-foreach ($signal as $key => $value) {
-	echo  $stationname[$key] .  " - Signal:" . $signal[$key] . "\n";
+
+        //echo 'true';
+    }else {
+        $str = substr($str,12);
+        $stationmac[] = $str;
+    }
+
+
+    //echo $str. "\n";
+
+
 }
 
+//var_dump($stationmacsnmp);
 
+
+/*foreach ($signal as $key => $value) {
+	echo  $stationname[$key] .  " // Signal:" . $signal[$key] . " // "." Mac Address: " . $stationmac[$key] ."\n";
+}*/
+
+var_dump($stationTxRate);
 
 
 
